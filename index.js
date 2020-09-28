c657c97bf086ae29e38df0ffccae31547e617999
Immanuel Ngari <imanga254@gmail.com>
2020-09-28 13:36:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo (name) {
 function happyCustomHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown (holiday, day) {
-  return `It\'s ${days} days until ${holiday}!`
+function holidayCountdown (day, holiday) {
+  return `It\'s ${day} days until ${holiday}!`
 }
